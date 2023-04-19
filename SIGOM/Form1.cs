13fc507a42a33aff19b12eaf92c8dc11bb92fff0
Gui-Angelo-Silva <guilherme_angelosilva@hotmail.com>
2023-04-19 19:39:47 -0300

    Primeiro commit

--- SIGOM/Form1.cs
+++ SIGOM/Form1.cs
@@ -16,5 +16,10 @@ namespace SIGOM
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
